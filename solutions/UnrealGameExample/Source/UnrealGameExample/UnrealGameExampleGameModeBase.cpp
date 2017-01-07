b6a537febc198c526211d321e4a960a032e085fa
Črtomir Drofenik <c3rtz0r@gmail.com>
2017-01-07 23:03:56 +0100

    Added example unreal engine game solution - needs unreal engine 4

---
+++ solutions/UnrealGameExample/Source/UnrealGameExample/UnrealGameExampleGameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealGameExample.h"
+#include "UnrealGameExampleGameModeBase.h"
+
+
+
+
---
+++ solutions/UnrealGameExample/Source/UnrealGameExample/UnrealGameExampleGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UnrealGameExampleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALGAMEEXAMPLE_API AUnrealGameExampleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
